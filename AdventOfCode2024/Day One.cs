1d6a74940197962b1e154bd149fbe3174aadeec4
joel.wright.cs@gmail.com <joel.wright.cs@gmail.com>
2024-12-02 18:38:11 +0000

    Add project files.

---
+++ AdventOfCode2024/Day One.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024
+{
+    internal class Day_One
+    {
+        internal static async void Solution()
+        {
+            List<int> leftColumn = new();
+            List<int> rightColumn = new();
+            StreamReader sr = new("DayOne.txt");
+            string line;
+            while((line = sr.ReadLine()) != null)
+            {
+                string[] lineParts = line.Split("   ");
+                leftColumn.Add(int.Parse(lineParts[0].Trim()));
+                rightColumn.Add(int.Parse(lineParts[1].Trim()));
+            }
+            int totalDistance = 
+        }
+    }      
+}
+
+
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2024
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Task.Run(() => Day_One.Solution());
+        }
+    }
+}

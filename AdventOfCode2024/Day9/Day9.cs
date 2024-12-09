dbe2fa96f8a619ad4100c157f7dd252adf808691
joel.wright.cs@gmail.com <joel.wright.cs@gmail.com>
2024-12-09 21:54:03 +0000

    Fix logic for Day 9 part 1

--- AdventOfCode2024/Day9/Day9.cs
+++ AdventOfCode2024/Day9/Day9.cs
@@ -11,53 +11,60 @@ namespace AdventOfCode2024
     internal static class Day9
     {
         static string puzzleInput = "";
+        static int currentFileId = 0;
         internal static void ReadFile()
         {
             puzzleInput = File.ReadAllText("Day9/Day9.txt").Trim();
         }
         internal static long Solution_One()
         {
-            List<char> disk = BuildDisk(puzzleInput);
+            List<int> disk = BuildDisk(puzzleInput);
             CompactDisk(disk);
             return CalculateChecksum(disk);
         }
-        internal static List<char> BuildDisk(string input)
+        internal static List<int> BuildDisk(string input)
         {
-            List<char> disk = new();
-            int fileID = 0;
+            List<int> disk = new();
             
             for (int i = 0; i < input.Length; i++)
             {
                 int length = int.Parse(input[i].ToString());
+                int currentNum = -1;
 
-                char currentChar = input[i];
-
-                if (i % 2 == 1) currentChar = '.';
+                if (i % 2 == 0)
+                {
+                    currentNum = currentFileId;
+                    currentFileId++;
+                }
 
                 for (int j = 0; j < length; j++)
                 {
-                    disk.Add(currentChar);
+                    disk.Add(currentNum);
                 }
             }
             return disk;
         }
-        internal static void CompactDisk(List<char> disk)
+        internal static void CompactDisk(List<int> disk)
         {
-            while (disk.Contains('.'))
+            for (int i = 0; i < disk.Count; i++)
             {
-                int left = disk.FindIndex(c => c == '.');
-                int right = disk.FindLast(Char.IsNumber);
-                disk[left] = disk[right];
-                disk.RemoveAt(right);
+                if (disk[i] == -1)
+                {
+                    int lastNum = disk.FindLastIndex(x => x > 0);
+                    if (i > lastNum) break;
+                    disk[i] = disk[lastNum];
+                    disk[lastNum] = -1;
+                }
             }
         }
-        internal static long CalculateChecksum(List<char> disk)
+        internal static long CalculateChecksum(List<int> disk)
         {
             long checkSum = 0;
-            for (int i = 0; i <  disk.Count; i++)
+            for (int i = 0; i < disk.Count; i++)
             {
+                if (disk[i] == -1) break;
                 {
-                    checkSum += i * long.Parse(disk[i].ToString());
+                    checkSum += i * disk[i];
                 }
             }
             return checkSum;

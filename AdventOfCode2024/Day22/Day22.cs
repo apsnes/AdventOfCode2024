d85062f89d3e184e393cdaaa816320b4ecad3c5c
apsnes <joel.wright.cs@gmail.com>
2024-12-22 21:25:06 +0000

    implement template for Day22

---
+++ AdventOfCode2024/Day22/Day22.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024
+{
+    internal static class Day22
+    {
+        static List<int> initialNumbers = new();
+        internal static void ReadFile()
+        {
+            foreach (var item in File.ReadAllLines("Day22/Day22.txt").ToList())
+            {
+                initialNumbers.Add(int.Parse(item.Trim()));
+            }
+            foreach (var item in initialNumbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        internal static long SumSecretNumbers()
+        {
+            long sum = 0;
+
+
+            return sum;
+        }
+    }
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -6,7 +6,6 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Day19.ReadFiles();
-        Console.WriteLine(Day19.ValidDesigns());
+        Day22.ReadFile();
     }
 }
